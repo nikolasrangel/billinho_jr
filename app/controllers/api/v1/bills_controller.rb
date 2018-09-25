f5d9d6c91f354f55b0e745f086b561a0e1d2ccd2
Nikolas Rangel <nikolas.rangel@gmail.com>
2018-09-25 00:51:10 -0300

    Criado PUT para as faturas e validacao dos atributos ao inserir e atualizar

--- app/controllers/api/v1/bills_controller.rb
+++ app/controllers/api/v1/bills_controller.rb
@@ -2,6 +2,9 @@ module Api
   module V1
     class BillsController < ApplicationController
 
+      rescue_from ActionController::ParameterMissing, with: :error_parameter_missing
+      rescue_from ActiveRecord::RecordNotFound, with: :error_not_found
+
       # Get all bills
 			def index
 				@bills = Bill.select(:id, :valor_fatura, :data_vencimento, :status, :enrollment_id).order("enrollment_id DESC");
@@ -21,8 +24,30 @@ module Api
         end
       end
 
+      # Update a specific bill by its ID
+      def update
+        @bill_data = params.require(:bill).permit(:status)
+
+        @bill = Bill.find(params[:id])
+
+        if @bill.update(@bill_data)
+          render json: {status: "Success", message: "Updated status for the bill's ID: #{params[:id]}.", data: @bill.as_json(only: [:id, :valor_fatura, :data_vencimento, :status, :enrollment_id])}, status: :ok
+        elsif
+          render json: {status: "Error", message: @bill.errors}, status: :not_found
+        end
+
+      end
+
+      def error_parameter_missing(error)
+        render json: {status: "Error", message: error}, status: :unprocessable_entity
+      end
+
       def error_request
         render json: {status: "Error", message: "Error in request syntax."}, status: :bad_request
+      end
+
+      def error_not_found
+        render json: {status: "Error", message: "Bill's ID not found."}, status: :not_found
       end
 
     end
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,6 @@
 class Bill < ApplicationRecord
   belongs_to :enrollment
   
+  validates_presence_of :valor_fatura, :data_vencimento, :enrollment_id, :status
+  validates :status, inclusion: { in: %w(Aberta Atrasada Paga) }
 end
